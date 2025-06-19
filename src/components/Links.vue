12911fc12fc320f5b3973e4ce6ed35848a81b98a
SoftLeaderGy <826044698@qq.com>
2025-06-19 18:00:45 +0800

    添加画布连接

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite,Mixcloud } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite,Mixcloud,DrawPolygon } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -79,7 +79,8 @@ const siteIcon = {
   ShareAlt,
   Slideshare,
   CookieBite,
-  Mixcloud
+  Mixcloud,
+  DrawPolygon
 };
 
 // 链接跳转
