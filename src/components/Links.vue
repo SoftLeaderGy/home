60568b7701f34969edc4600b2563ad554b6497c3
SoftLeaderGy <826044698@qq.com>
2025-06-19 17:39:28 +0800

    添加Uptime Kuma连接

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite,Mixcloud } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -78,7 +78,8 @@ const siteIcon = {
   Gamepad,
   ShareAlt,
   Slideshare,
-  CookieBite
+  CookieBite,
+  Mixcloud
 };
 
 // 链接跳转
