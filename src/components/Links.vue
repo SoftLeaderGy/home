9894d203d87309c1d8adc40e23ea6bdbfa364afc
SoftLeaderGy <826044698@qq.com>
2025-06-24 13:58:54 +0800

    添加奈飞工厂链接

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,6 +44,7 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite,Mixcloud,DrawPolygon } from "@vicons/fa"; // 注意使用正确的类别
+import { BrandNetflix} from "@vicons/tabler";
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -80,7 +81,8 @@ const siteIcon = {
   Slideshare,
   CookieBite,
   Mixcloud,
-  DrawPolygon
+  DrawPolygon,
+  BrandNetflix
 };
 
 // 链接跳转
