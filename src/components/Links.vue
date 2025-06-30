d494a3b53e84f859bb205a4976f8d5f4ef22bea8
SoftLeaderGy <826044698@qq.com>
2025-06-30 14:54:32 +0800

    添加音乐链接

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite,Mixcloud,DrawPolygon } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Weibo,YandexInternational,ExternalLinkAlt,Film,Searchengin,Gamepad,ShareAlt,Slideshare,CookieBite,Mixcloud,DrawPolygon,Music } from "@vicons/fa"; // 注意使用正确的类别
 import { BrandNetflix} from "@vicons/tabler";
 import { ServerProxy,Report} from "@vicons/carbon";
 import { mainStore } from "@/store";
@@ -85,7 +85,8 @@ const siteIcon = {
   DrawPolygon,
   BrandNetflix,
   ServerProxy,
-  Report
+  Report,
+  Music
 };
 
 // 链接跳转
